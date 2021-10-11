35530fcfbfb0996e19114812c2f00077a53c30d9
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-10-11 09:31:53 -0500

    Add createList function to Add.js component

--- src/Add.js
+++ src/Add.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { getToken, words } from '@the-collab-lab/shopping-list-utils';
 
 export function Add() {
+  function createList() {
+    // check if there's a user token in localStorage
+    // if user token:
+    const userToken = localStorage.getItem('userToken');
+    console.log(userToken);
+    //-- check if there is a list
+    // if no user token:
+    //-- create a token
+    //-- store the token in localStorage
+    //-- redirect user to the "list" view
+  }
   return (
     <div>
-      <button className="add">Create a new item</button>
+      <button className="add" onClick={createList}>
+        Create a new item
+      </button>
     </div>
   );
 }
