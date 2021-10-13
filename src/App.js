ffd74aecaf672d8121a4e63b516a3129daafdbeb
Gabriela Niño <gabynr@gmail.com>
2021-10-13 08:34:47 -0400

    Refactorization of code in component App.
    Co-authored-by: gabynr <16828751+gabynr@users.noreply.github.com>
    Co-authored-by: andreamartz <14165788+andreamartz@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,57 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { db } from './lib/firebase.js';
+import React from 'react';
 import './App.css';
-import {
-  collection,
-  getDocs,
-  query,
-  onSnapshot,
-  doc,
-  setDoc,
-} from '@firebase/firestore';
-import {
-  BrowserRouter as Router,
-  Link,
-  NavLink,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { List } from './List';
 import { Add } from './Add';
 import { Home } from './Home';
-import { NavigationMenu } from './NavigationMenu';
 
 function App() {
-  /*const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const response = collection(db, 'users');
-      const usersList = query(response);
-      // console.log(usersList.users.docs);
-      // setUsers(data)
-      const unsubscribe = onSnapshot(usersList, (querySnapshot) => {
-        const users = querySnapshot.docs.reduce((acc, doc) => {
-          const { name } = doc.data();
-          const id = doc.id;
-          return [...acc, { id, name }];
-        }, []);
-
-        setUsers(users);
-      });
-    };
-
-    fetchUsers();
-  }, []);
-
-  const handleClick = async () => {
-    await setDoc(doc(db, 'cities', 'LA'), {
-      name: 'Los Angeles',
-      state: 'CA',
-      country: 'USA',
-    });
-  };
-*/
   return (
     <Router>
       {/* A <Switch> looks through its children <Route>s and
@@ -67,7 +21,6 @@ function App() {
           <Add />
         </Route>
       </Switch>
-      <NavigationMenu />
     </Router>
   );
 }
