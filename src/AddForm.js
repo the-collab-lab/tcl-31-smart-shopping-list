c209f34dbcfbc008e2b89a7019f3f32753f2fe34
bajancode <victory.web.development@gmail.com>
2021-10-14 10:59:21 -0400

    Controlled componenent and removed use effect function

--- src/AddForm.js
+++ src/AddForm.js
@@ -13,7 +13,7 @@ const token = 'plushy cuny idiom';
 
 function AddForm() {
   const [item, setItem] = useState('');
-  const [days, setDays] = useState(0);
+  const [days, setDays] = useState('7');
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -36,14 +36,32 @@ function AddForm() {
         />
       </label>
       <div onChange={(e) => setDays(e.target.value)}>
-        <input type="radio" id="soon" name="days" value={7} />
+        <input
+          type="radio"
+          id="soon"
+          name="days"
+          value="7"
+          checked={days === '7'}
+        />
         <label htmlFor="soon">Soon</label>
-        <input type="radio" id="kind-of-soon" name="days" value={14} />
+        <input
+          type="radio"
+          id="kind-of-soon"
+          name="days"
+          value="14"
+          checked={days === '14'}
+        />
         <label htmlFor="kind-of-soon">Kind of Soon</label>
-        <input type="radio" id="not-soon" name="days" value={30} />
+        <input
+          type="radio"
+          id="not-soon"
+          name="days"
+          value="30"
+          checked={days === '30'}
+        />
         <label htmlFor="not-soon">Not Soon</label>
       </div>
-      <input type="submit" value="Add Item" />
+      <button type="submit">Add Item</button>
     </form>
   );
 }
--- src/App.js
+++ src/App.js
@@ -21,26 +21,6 @@ import { Add } from './Add';
 function App() {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const response = collection(db, 'users');
-      const usersList = query(response);
-      // console.log(usersList.users.docs);
-      // setUsers(data)
-      const unsubscribe = onSnapshot(usersList, (querySnapshot) => {
-        const users = querySnapshot.docs.reduce((acc, doc) => {
-          const { name } = doc.data();
-          const id = doc.id;
-          return [...acc, { id, name }];
-        }, []);
-
-        setUsers(users);
-      });
-    };
-
-    fetchUsers();
-  }, []);
-
   const handleClick = async () => {
     await setDoc(doc(db, 'cities', 'LA'), {
       name: 'Los Angeles',
