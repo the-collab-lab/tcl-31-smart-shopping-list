a4f6bb34d079d848a88a57edc23fbf652f81d7c3
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-10-17 17:48:55 -0500

    Show list items filtered by token
    Co-authored-by: andreamartz 14165788+andreamartz@users.noreply.github.com
    Co-authored-by: aidantorrence aidan.torrence@gmail.com

--- src/List.js
+++ src/List.js
@@ -13,6 +13,8 @@ import { NavigationMenu } from './NavigationMenu';
 export function List() {
   const [items, setItems] = useState([]);
 
+  const token = localStorage.getItem('token');
+
   useEffect(() => {
     const fetchItems = async () => {
       const response = collection(db, 'shopping-list');
@@ -20,9 +22,9 @@ export function List() {
 
       const unsubscribe = onSnapshot(itemList, (querySnapshot) => {
         const items = querySnapshot.docs.reduce((acc, doc) => {
-          const { name } = doc.data();
+          const { name, userToken } = doc.data();
           const id = doc.id;
-          return [...acc, { id, name }];
+          return [...acc, { id, name, userToken }];
         }, []);
 
         console.log(items);
@@ -33,10 +35,18 @@ export function List() {
     fetchItems();
   }, []);
 
+  console.log(
+    'filtered items',
+    items.filter((item) => item.userToken === token),
+  );
+
   return (
     <div>
       <ul className="list">
-        {items && items.map((item) => <li key={item.id}>{item.name}</li>)}
+        {items &&
+          items
+            .filter((item) => item.userToken === token)
+            .map((item) => <li key={item.id}>{item.name}</li>)}
       </ul>
       <NavigationMenu />
     </div>
