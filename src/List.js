a1918d570f3d1e11b95d4cf6f3f931fc6b7c3d86
bajancode <victory.web.development@gmail.com>
2021-11-19 00:32:58 -0400

    tried to add radio buttons and text input using material UI

--- src/List.js
+++ src/List.js
@@ -15,13 +15,13 @@ import { NavigationMenu } from './NavigationMenu';
 import { useHistory } from 'react-router-dom';
 import DeleteButton from './DeleteButton';
 import AddForm from './AddForm';
-import {
-  FormControl,
-  FormLabel,
-  RadioGroup,
-  FormControlLabel,
-  Radio,
-} from '@mui/material';
+// import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio  } from '@mui/material';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import Radio from '@mui/material/Radio';
+import TextField from '@mui/material/TextField';
 
 const convertToDays = (num) => Math.round(num / 1000 / 60 / 60 / 24);
 
@@ -179,6 +179,10 @@ export function List() {
 
   return items.length ? (
     <>
+      <AddForm />
+      {/* Attempted TextField Add */}
+      <TextField />
+      {/* Attempted radio buttons add copied directly from documentation */}
       <FormControl component="fieldset">
         <FormLabel component="legend">Gender</FormLabel>
         <RadioGroup
@@ -191,8 +195,6 @@ export function List() {
           <FormControlLabel value="other" control={<Radio />} label="Other" />
         </RadioGroup>
       </FormControl>
-
-      <AddForm />
 
       <label htmlFor="filterItems">Filter items:</label>
       <input
@@ -233,6 +235,7 @@ export function List() {
               );
             })}
       </ul>
+
       <NavigationMenu />
     </>
   ) : (
