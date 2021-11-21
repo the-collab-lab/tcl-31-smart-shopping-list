086a662445b7d9ff5f8005008294f013c340de28
bajancode <victory.web.development@gmail.com>
2021-11-20 22:26:31 -0400

    add item button material UI added, began centering AddForm items

--- src/AddForm.js
+++ src/AddForm.js
@@ -15,9 +15,10 @@ import {
   Radio,
   TextField,
   Box,
-  FormLabel,
+  Button,
 } from '@mui/material';
 import { lightGreen, orange, red } from '@mui/material/colors';
+import './App.css';
 
 function AddForm() {
   const [item, setItem] = useState('');
@@ -82,90 +83,92 @@ function AddForm() {
 
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
-      <h3>What do you need?</h3>
-      {/* Begin the MateriaUI */}
-      <Box
-        sx={{
-          width: 368,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          '& > :not(style)': { m: 1 },
-        }}
-      >
-        <TextField
-          // helperText="What do you need"
-          id="demo-helper-text-aligned"
-          onChange={(e) => setItem(e.target.value)}
-          label="type your item here"
-        />
-      </Box>
-      {/* finish the materia UI*/}
+      <div id="add-form-content">
+        <h2>What do you need?</h2>
+        {/* Begin the MaterialUI */}
+        <Box
+          sx={{
+            width: 368,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            '& > :not(style)': { m: 1 },
+          }}
+        >
+          <TextField
+            // helperText="What do you need"
+            id="demo-helper-text-aligned"
+            onChange={(e) => setItem(e.target.value)}
+            label="type your item here"
+          />
+        </Box>
+        {/* finish the materia UI*/}
 
-      <div>
-        <FormControl component="fieldset">
-          <FormLabel component="legend">When do you need it?</FormLabel>
-          <RadioGroup
-            row
-            aria-label="days"
-            value="7"
-            name="radio-buttons-group"
-            onChange={handleChange}
-          >
-            <FormControlLabel
+        <div>
+          <FormControl component="fieldset">
+            <h3>When do you need it?</h3>
+
+            <RadioGroup
+              row
+              aria-label="days"
               value="7"
-              control={
-                <Radio
-                  id="soon"
-                  name="days"
-                  checked={days === 7}
-                  sx={{
-                    color: lightGreen[800],
-                    '&.Mui-checked': {
-                      color: lightGreen[500],
-                    },
-                  }}
-                />
-              }
-              label="This week"
-            />
-            <FormControlLabel
-              value="14"
-              control={
-                <Radio
-                  id="kind-of-soon"
-                  name="days"
-                  checked={days === 14}
-                  sx={{
-                    color: orange[800],
-                    '&.Mui-checked': {
-                      color: orange[500],
-                    },
-                  }}
-                />
-              }
-              label="Next week"
-            />
-            <FormControlLabel
-              value="30"
-              control={
-                <Radio
-                  id="not-soon"
-                  name="days"
-                  checked={days === 30}
-                  sx={{
-                    color: red[800],
-                    '&.Mui-checked': {
-                      color: red[500],
-                    },
-                  }}
-                />
-              }
-              label="Next month"
-            />
-          </RadioGroup>
-        </FormControl>
-        {/* Old Radio buttons before Material UI added, delete if working well!
+              name="radio-buttons-group"
+              onChange={handleChange}
+            >
+              <FormControlLabel
+                value="7"
+                control={
+                  <Radio
+                    id="soon"
+                    name="days"
+                    checked={days === 7}
+                    sx={{
+                      color: lightGreen[800],
+                      '&.Mui-checked': {
+                        color: lightGreen[500],
+                      },
+                    }}
+                  />
+                }
+                label="This week"
+              />
+              <FormControlLabel
+                value="14"
+                control={
+                  <Radio
+                    id="kind-of-soon"
+                    name="days"
+                    checked={days === 14}
+                    sx={{
+                      color: orange[800],
+                      '&.Mui-checked': {
+                        color: orange[500],
+                      },
+                    }}
+                  />
+                }
+                label="Next week"
+              />
+              <FormControlLabel
+                value="30"
+                control={
+                  <Radio
+                    id="not-soon"
+                    name="days"
+                    checked={days === 30}
+                    sx={{
+                      color: red[800],
+                      '&.Mui-checked': {
+                        color: red[500],
+                      },
+                    }}
+                  />
+                }
+                label="Next month"
+              />
+            </RadioGroup>
+          </FormControl>
+          {/* Old Radio buttons before Material UI added, delete if working well!
         <input
           type="radio"
           id="soon"
@@ -193,12 +196,16 @@ function AddForm() {
           onChange={handleChange}
         />
         <label htmlFor="not-soon">Next month</label> */}
-      </div>
-      <button type="submit">Add Item</button>
+        </div>
+        <Button variant="outlined" type="submit" id="submit-item">
+          {' '}
+          + Add Item
+        </Button>
 
-      {errorMessage !== '' && (
-        <div className="error-message">{errorMessage}</div>
-      )}
+        {errorMessage !== '' && (
+          <div className="error-message">{errorMessage}</div>
+        )}
+      </div>
     </form>
   );
 }
